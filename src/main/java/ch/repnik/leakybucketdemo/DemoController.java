3979d9f903f432455aba586f0d2fad38f47f630d
Thomas Repnik <thomas.repnik@gmx.ch>
2024-03-16 12:56:38 +0100

    IP Filtering and Caching added

--- src/main/java/ch/repnik/leakybucketdemo/DemoController.java
+++ src/main/java/ch/repnik/leakybucketdemo/DemoController.java
@@ -14,23 +14,9 @@ import java.time.Duration;
 @RestController
 public class DemoController {
 
-    private final LocalBucket bucket;
-
-    public DemoController() {
-        Bandwidth limit = Bandwidth.builder().capacity(5).refillGreedy(2, Duration.ofMinutes(1)).build();
-        this.bucket = Bucket.builder()
-                .addLimit(limit)
-                .build();
-    }
-
     @GetMapping("/hello")
     public ResponseEntity<String> helloWorld() {
-
-        if (bucket.tryConsume(1)) {
-            return ResponseEntity.ok("Hello World");
-        }
-
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        return ResponseEntity.ok("Hello World");
     }
 
 }
--- src/main/java/ch/repnik/leakybucketdemo/LeakyBucketDemoApplication.java
+++ src/main/java/ch/repnik/leakybucketdemo/LeakyBucketDemoApplication.java
@@ -2,7 +2,9 @@ package ch.repnik.leakybucketdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class LeakyBucketDemoApplication {
 
